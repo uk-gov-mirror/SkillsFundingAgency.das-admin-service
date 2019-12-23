c6ae9b5e5825cbb8e13da4ad3f1e8364adecd949
Dave Read <david.read@digital.education.gov.uk>
2019-12-23 15:39:12 +0000

    APR-1161 add authorize attribute to spike controller

--- src/SFA.DAS.AdminService.Web/Controllers/Roatp/AssessmentSpikeController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/AssessmentSpikeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
 using System;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace SFA.DAS.AdminService.Web.Controllers.Roatp
 {
-    
+    [Authorize]
     public class AssessmentSpikeController : Controller
     {
         private readonly IQnaApiClient _qnaApiClient;
